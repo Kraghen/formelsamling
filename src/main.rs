7762eee324e7e0995dbcce2cbd3f49ad68b97d5b
kraghen <kraghen2007@gmail.com>
2022-06-15 09:41:41 +0200

    Fjernede alt

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,5 @@ enum InfoType {
 }
 
 fn main() {
-    let inf = InfoContainer {name: String::from("Firkant"), content: vec![InfoType::Text(String::from("Hej"))]};
-
-    let not_valid = &String::from("Der er mere");
-
-    let out = match &inf.content[0] {
-        InfoType::Text(s) => s,
-        _ => {
-            not_valid
-        }
-    };
-
-    println!("{}", out)
 
 }
